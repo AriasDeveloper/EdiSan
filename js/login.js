5c32447997a10337f81e28aa544d7541f394eb30
Josué <leonardoarias137@gmail.com>
2026-07-12 23:10:52 -0400

    no recuerda

--- js/login.js
+++ js/login.js
@@ -1,14 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const usuarioGuardado = localStorage.getItem('usuarioGuardado');
-    const inputUsuario = document.getElementById('usuario');
-    const checkboxRecordar = document.getElementById('recordarme');
-
-    // Si encontramos un usuario en el "bloc de notas", lo ponemos en el formulario
-    if (usuarioGuardado) {
-        inputUsuario.value = usuarioGuardado;
-        checkboxRecordar.checked = true; // Marcamos el checkbox como activo
-    }
-});
 document.getElementById('formlogin').addEventListener('submit', async function(e) {
   e.preventDefault();
 
@@ -24,13 +13,6 @@ document.getElementById('formlogin').addEventListener('submit', async function(e
     const datos = await respuesta.json();
 
     if (datos.autenticado === 2) {
-        // Si el checkbox está marcado, guardamos el usuario
-        if (document.getElementById('recordarme').checked) {
-            localStorage.setItem('usuarioGuardado', usuario);
-        } else {
-            // Si no está marcado, borramos cualquier usuario previo
-            localStorage.removeItem('usuarioGuardado');
-        }
       window.location.href = 'admin.html';
     } else if (datos.autenticado === 1) {
       window.location.href = 'cliente.html';
